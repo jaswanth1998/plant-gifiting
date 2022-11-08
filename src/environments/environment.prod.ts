26cb629cc6e4a77e8e73bfdc19b8af6641f04982
“jaswanth1998” <jaswanthtata@gmail.com>
2022-11-09 01:40:49 +0530

    fix: url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // API_ENDPOINT : 'http://m-hariomtech-pub-alb-439867190.ap-south-1.elb.amazonaws.com:8003'
-  API_ENDPOINT : 'http://3.111.204.205:8003'
+  API_ENDPOINT : 'http://m-hariomtech-pub-alb-439867190.ap-south-1.elb.amazonaws.com:8003'
+  // API_ENDPOINT : 'http://3.111.204.205:8003'
 };
